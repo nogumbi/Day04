36fe8ad914cf8145632bf1c50f5be058bd0d82bf
Nobantu Gumbi <nogumbi@c4r18s1.wethinkcode.co.za>
2020-07-15 13:20:00 +0200

    Power

--- ex02/ft_iterative_power.c
+++ ex02/ft_iterative_power.c
@@ -6,16 +6,17 @@
 /*   By: nogumbi <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/13 09:00:07 by nogumbi           #+#    #+#             */
-/*   Updated: 2020/07/13 11:17:42 by nogumbi          ###   ########.fr       */
+/*   Updated: 2020/07/15 13:17:03 by nogumbi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int ft_iterative_power(int nb, int power)
+int 		ft_iterative_power(int nb, int power)
 {
-	int pwr = 1;
+	int pwr;
+   	pwr 	= 1;
 
 	if( power < 0)
-	{
+	
 		return 0;
 	}
 	while(power--)
